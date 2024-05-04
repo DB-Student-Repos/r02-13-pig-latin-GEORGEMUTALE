cbd81983c044068226e270d924961051eb1e4395
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-04 11:20:02 +0200

    Pig latin word

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,35 @@
 pub fn translate(input: &str) -> String {
-    unimplemented!(
-        "Using the Pig Latin text transformation rules, convert the given input '{input}'"
-    );
+    input
+        .split_whitespace()
+        .map(translate_word_to_pig_latin)
+        .collect::<Vec<String>>()
+        .join(" ")
 }
+
+fn translate_word_to_pig_latin(word: &str) -> String {
+    let n = consonant_chars(word);
+    let (consonant, other) = word.split_at(n);
+
+    format!("{}{}ay", other, consonant)
+}
+
+fn consonant_chars(word: &str) -> usize {
+    if word.is_empty()
+        || word.starts_with('a')
+        || word.starts_with('e')
+        || word.starts_with('i')
+        || word.starts_with('o')
+        || word.starts_with('u')
+        || word.starts_with("xr")
+        || word.starts_with("yt")
+    {
+        0
+    } else if word.starts_with("qu") {
+        2
+    } else if word.chars().nth(1) == Some('y') {
+        1
+    } else {
+        1 + consonant_chars(&word[1..])
+    }
+}
+
